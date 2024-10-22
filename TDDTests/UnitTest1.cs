dbf3c926a5033cd9857ccf3de9760366da4a578c
Darya <adw_msk@list.ru>
2024-10-22 23:08:59 +0300

    первый коммит

---
+++ TDDTests/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+[TestClass]
+public class TDDTests
+{
+    private TestClass _analyzer;
+
+    [TestInitialize]
+    public void Setup()  //  метод будет выполнен перед каждым тестом
+    {
+        _analyzer = new TestClass(); // инициализация экземпляра
+    }
+
+    [TestMethod]
+    public void TestFindShortestWord()
+    {
+        Assert.AreEqual("a", _analyzer.FindShortestWord("apple banana a cherry")); // метод находит самое короткое слово в строке
+        Assert.AreEqual(string.Empty, _analyzer.FindShortestWord(string.Empty));  
+        Assert.AreEqual(string.Empty, _analyzer.FindShortestWord("   "));
+    }
+
+    [TestMethod]
+    public void TestCountWords()
+    {
+        Assert.AreEqual(4, _analyzer.CountWords("apple banana a cherry"));  //  метод правильно считает количество слов в строке
+        Assert.AreEqual(0, _analyzer.CountWords(string.Empty));
+        Assert.AreEqual(0, _analyzer.CountWords("   "));
+    }
+
+    [TestMethod]
+    public void TestCountCharacters()
+    {
+        Assert.AreEqual(22, _analyzer.CountCharacters("apple banana a cherry"));  // метод правильно считает количество символов в строке
+        Assert.AreEqual(0, _analyzer.CountCharacters(string.Empty));
+    }
+
+    [TestMethod]
+    public void TestFindNinthCharacter()
+    {
+        Assert.AreEqual('i', _analyzer.FindNinthCharacter("abcdefghij")); // метод возвращает 9 символ строки
+        Assert.IsNull(_analyzer.FindNinthCharacter("short"));
+    }
+
+    [TestMethod]
+    public void TestCountDigits()
+    {
+        Assert.AreEqual(5, _analyzer.CountDigits("abc12345def678"));  // метод правильно считает количество цифр в строке
+        Assert.AreEqual(0, _analyzer.CountDigits("abcde"));
+    }
+
+    [TestMethod]
+    public void TestCountConsecutiveDigits()
+    {
+        Assert.AreEqual(5, _analyzer.CountConsecutiveDigits("abc12345def678")); // метод находит максимальное количество идущих цифр
+        Assert.AreEqual(0, _analyzer.CountConsecutiveDigits("abcde"));
+    }
+
+    [TestMethod]
+    public void TestCountOccurrencesOfAm()
+    {
+        Assert.AreEqual(2, _analyzer.CountOccurrencesOfAm("мама мыла раму, а ам")); // метод считает количество вхождений ам в строке
+        Assert.AreEqual(0, _analyzer.CountOccurrencesOfAm("мама мыла раму"));
+    }
+}
---
+++ TestClass/TestClass.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System;
+
+public class TestClass
+{
+    public string FindShortestWord(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) // проверка на пустоту
+            return string.Empty;
+
+        return input.Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                    .OrderBy(word => word.Length)
+                    .FirstOrDefault(); // Разделение строки на слова, сортировка по длине и возврат первого слова
+    }
+
+    public int CountWords(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return 0;
+
+        return input.Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length; // Разделение строки на слова и возврат их количества
+    }
+
+    public int CountCharacters(string input)
+    {
+        return string.IsNullOrEmpty(input) ? 0 : input.Length; // Возвращает 0 если строка пустая
+    }
+
+    public char? FindNinthCharacter(string input)
+    {
+        if (string.IsNullOrEmpty(input) || input.Length < 9)
+            return null;
+
+        return input[8]; // 9-ый символ (индекс 8)
+    }
+
+    public int CountDigits(string input)
+    {
+        return string.IsNullOrEmpty(input) ? 0 : input.Count(char.IsDigit);
+    }
+
+    public int CountConsecutiveDigits(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return 0;
+
+        int maxCount = 0; // Переменная цифр
+        int currentCount = 0; //Переменная для подсчета текущего количества подряд идущих цифр
+
+        foreach (char c in input) // Перебор каждого символа в строке
+        {
+            if (char.IsDigit(c))
+            {
+                currentCount++;
+                if (currentCount > maxCount)
+                    maxCount = currentCount; // обновление максимального значения
+            }
+            else
+            {
+                currentCount = 0; //сброс
+            }
+        }
+
+        return maxCount;
+    }
+
+    public int CountOccurrencesOfAm(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return 0;
+
+        return (input.Length - input.Replace("ам", "").Length) / "ам".Length;
+    }
+}
